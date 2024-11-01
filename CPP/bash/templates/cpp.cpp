e3918be942d15c28354ef0fc9915db73eec3c09e
QuickFixDev <juan.hernandez.mirabile@gmail.com>
2024-10-31 23:21:24 -0600

    modify metadata design

--- CPP/bash/templates/cpp.cpp
+++ CPP/bash/templates/cpp.cpp
@@ -1,14 +1,16 @@
 /*
- * --- Metadata ---
- *
- * Practice name:       lorem
- * Suggested filename:  main.cpp
- * Filename:            main.cpp
- * Author:              Juan Pablo Hernandez Ramirez
- * Date:                2024-10-31
- * Version:             1.0.0
- * Description:         lorem
- */
+---------------------------------------------------
+Metadata
+---------------------------------------------------
+Practice name:       lorem
+Suggested filename:  main.cpp
+Filename:            main.cpp
+Author:              Juan Pablo Hernandez Ramirez
+Date:                2024-10-31
+Version:             1.0.0
+Description:         lorem
+---------------------------------------------------
+*/
 
 
 #include <stdio.h>
