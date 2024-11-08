4496bdf5d34f56a701cb46f40acb75844f5a55eb
QuickFixDev <juan.hernandez.mirabile@gmail.com>
2024-11-07 23:20:58 -0600

    add forgotten metadata

--- CPP/parcial_2/P10/S1/sueldoMe.cpp
+++ CPP/parcial_2/P10/S1/sueldoMe.cpp
@@ -2,13 +2,12 @@
 +---------------------------------------------------+
 | Metadata                                          |
 +--------------------+------------------------------+
-| Practice name      | lorem                        |
-| Suggested filename | main.cpp                     |
-| Filename           | main.cpp                     |
+| Practice name      | Sueldo mas bajo              |
+| Suggested filename | sueldoMenor.cpp              |
+| Filename           | sueldoMe.cpp                 |
 | Author             | Juan Pablo Hernandez Ramirez |
-| Date               | 2024-10-31                   |
+| Date               | 2024-10-30                   |
 | Version            | 1.0.0                        |
-| Description        | lorem                        |
 +--------------------+------------------------------+
 */
 
--- CPP/parcial_2/P10/S2/genRand.cpp
+++ CPP/parcial_2/P10/S2/genRand.cpp
@@ -2,15 +2,15 @@
 +---------------------------------------------------+
 | Metadata                                          |
 +--------------------+------------------------------+
-| Practice name      | lorem                        |
-| Suggested filename | main.cpp                     |
-| Filename           | main.cpp                     |
+| Practice name      | Generar n numeros aleatorios |
+| Suggested filename | generarAleatorios.cpp        |
+| Filename           | genRand.cpp                  |
 | Author             | Juan Pablo Hernandez Ramirez |
-| Date               | 2024-10-31                   |
+| Date               | 2024-10-30                   |
 | Version            | 1.0.0                        |
-| Description        | lorem                        |
 +--------------------+------------------------------+
 */
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
